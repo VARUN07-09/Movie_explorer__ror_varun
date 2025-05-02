49a98faa5252ce986ef9336b61b3b274439e63f4
VARUN07-09 <varun91thakur@gmail.com>
2025-05-02 12:04:53 +0530

    [VARUN] MOD: Fixed subscription controler for admin

--- app/admin/subscription_plans.rb
+++ app/admin/subscription_plans.rb
@@ -3,14 +3,14 @@ ActiveAdmin.register SubscriptionPlan do
 
   filter :name
   filter :price
-  filter :duration
+  filter :duration_months
   filter :plan_type
 
   form do |f|
     f.inputs do
       f.input :name
       f.input :price
-      f.input :duration
+      f.input :duration_months
       f.input :plan_type, as: :select, collection: SubscriptionPlan.plan_types.keys
     end
     f.actions
